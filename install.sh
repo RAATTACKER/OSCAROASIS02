b6171b087128bfbf9608352ea93ec07232d5147b
RAATTACKER <50120118+RAATTACKER@users.noreply.github.com>
2019-04-30 04:54:34 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Auto Install Tools <3
+# Coded By ØSC4R O`4S1S
+# SEMOGA BERGUNA :)
+
+# Bersihkan Layar
+clear
+
+termux-setup-storage
+
+pkg install update -y
+pkg install upgrade -y
+pkg update && pkg upgrade -y
+apt update && apt upgrade -y
+pkg install nano -y
+pkg install unzip -y
+pkg install zip -y
+pkg install unrar -y
+pkg install mpd -y
+pkg install lolcat -y
+pkg install figlet -y
+pkg install tree -y
+pkg install ncmpcpp -y
+pkg install libcurl -y
+pkg install wget -y
+pkg install curl -y
+pkg install openssl curl -y
+pkg install python -y
+pkg install python2 -y
+pkg install pip
+pip install --upgrade pip
+pip2 install mechanize
+pkg install aria2 -y
+pip2 install beautifulsoup
+pkg install php -y
+pkg install mc -y
+pkg install tor -y
+pkg install git -y
+pkg install python pip install neovim
+pkg install neovim -y
+fi
+
+if [ $act = 22 ] || [ $act = 22  ]
+then
+echo " TERIMA KASIH SUDAH MENGINSTALL NYA "
+sleep 1
+echo " BY: ØSC4R O`4S1S "
+sleep 1
+exit
